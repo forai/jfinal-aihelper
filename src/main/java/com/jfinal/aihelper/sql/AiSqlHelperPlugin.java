84049b427ff95094ae744089818105eaab628b77
tj <forai@163..com>
2016-08-31 09:35:04 +0800

    commit by tj

---
+++ src/main/java/com/jfinal/aihelper/sql/AiSqlHelperPlugin.java
@@ -0,0 +1,20 @@
+package com.jfinal.aihelper.sql;
+
+import com.jfinal.plugin.IPlugin;
+
+/**
+ * Created by tangjing on 2016/8/31.9:34
+ */
+public class AiSqlHelperPlugin implements IPlugin {
+
+
+    @Override
+    public boolean start() {
+        return false;
+    }
+
+    @Override
+    public boolean stop() {
+        return false;
+    }
+}
