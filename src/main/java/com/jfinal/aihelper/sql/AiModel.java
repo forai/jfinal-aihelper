03615a57b910261a2b5e01cfa19c8479385e98c0
tj <forai@163..com>
2016-09-05 17:19:10 +0800

    commit by tj

--- src/main/java/com/jfinal/aihelper/sql/AiModel.java
+++ src/main/java/com/jfinal/aihelper/sql/AiModel.java
@@ -3,12 +3,18 @@ package com.jfinal.aihelper.sql;
 import com.jfinal.plugin.activerecord.Model;
 import com.jfinal.plugin.activerecord.Page;
 
+import java.util.List;
+
 /**
- * Created by Administrator on 2016/1/21.10:22
+ * Created by tangjing on 2016/1/21.10:22
  */
 public abstract class AiModel<M extends AiModel<M>> extends Model<M> {
 
     public Page<M> paginate(SqlParam sqlParam){
         return super.paginate(sqlParam.pageNum(), sqlParam.pageSize(), sqlParam.getSelect(), sqlParam.getSqlStr(), sqlParam.argsAry());
+    }
+
+    public List<M> find(SqlParam sqlParam){
+        return super.find(sqlParam.getFullSql(), sqlParam.argsAry());
     }
 }
