560e1a8d9ee49092c7ddef06c8b5d31b1d8c6d2a
tj <forai@163..com>
2016-09-06 10:39:28 +0800

    commit by tj

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,8 +1,6 @@
-import com.jfinal.aihelper.sql.AiSqlHelperPlugin;
-import com.jfinal.aihelper.sql.ISqlBuilder;
-import com.jfinal.aihelper.sql.ParamMap;
-import com.jfinal.aihelper.sql.SqlParam;
+import com.jfinal.aihelper.sql.*;
 import com.jfinal.aihelper.sql.builer.MySqlBuilder;
+import com.jfinal.aihelper.sql.builer.OracleBuilder;
 
 import java.util.Date;
 
@@ -15,6 +13,8 @@ public class Demo {
 //        ISqlBuilder sqlBuilder = new OracleBuilder();
         AiSqlHelperPlugin p = new AiSqlHelperPlugin(sqlBuilder);
         p.start();
+        AiSqlHelperPlugin p2 = new AiSqlHelperPlugin(SqlBuilderManager.ORACLE, new OracleBuilder());
+        p2.start();
         SqlParam sp = SqlParam.create(new ParamMap().put("age",17).put("addr","北京路").put("time",new Date()));
         sp.setSelect("select t1.* where 1=1");
         sp.alias("t1");
@@ -24,13 +24,24 @@ public class Demo {
         sp.search("addr", "addr");
         sp.dateAfter("start_time","time");
         sp.dateBefore("end_time","time");
-//        sp.orderAsc("create_time");
         sp.orderBy("t1.start_time,t1.age asc");
         System.out.println(sp.getFullSql());
         for(Object ags:sp.argsAry()){
             System.out.println(ags);
         }
-//        System.out.println(sp.argsAry().toString());
+        System.out.println("-------------------------------------------------");
+        sp.setSql(new StringBuffer());//清空前面一个sql的内容
+        sp.use(SqlBuilderManager.ORACLE);
+        sp.setSelect("select t1.* where 1=1");
+        sp.alias("t1");
+        sp.and("age","age");
+        sqlBuilder.and(sp.getSql(),"t1","name","tj",sp.getArgs());//直接使用sqlbuilder也可以
+        sp.search(new String[]{"home_addr","company_addr"},"addr");
+        sp.search("addr", "addr");
+        sp.dateAfter("start_time","time");
+        sp.dateBefore("end_time","time");
+        sp.orderBy("t1.start_time,t1.age asc");
+        System.out.println(sp.getFullSql());
     }
 
 }
