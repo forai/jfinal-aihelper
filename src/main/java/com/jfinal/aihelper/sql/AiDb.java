ce55036af767399572b210b0295b2448a6a090ec
tj <forai@163.com>
2016-11-10 10:41:15 +0800

    AiDb新增了 batchDel方法

--- src/main/java/com/jfinal/aihelper/sql/AiDb.java
+++ src/main/java/com/jfinal/aihelper/sql/AiDb.java
@@ -1,8 +1,7 @@
 package com.jfinal.aihelper.sql;
 
-import com.jfinal.plugin.activerecord.Db;
-import com.jfinal.plugin.activerecord.Page;
-import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.activerecord.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -29,5 +28,25 @@ public class AiDb{
     public static List<Record> find(SqlParam sqlParam){
         return Db.find(sqlParam.getFullSql(), sqlParam.argsAry());
     }
+
+
+    /**
+     * 批量删除
+     * 适用单个ID的表
+     * @param cls 类
+     * @param ids id数组
+     * @return
+     */
+    public static int batchDel(Class<? extends Model> cls,Object[] ids){
+        Table table = TableMapping.me().getTable(cls);
+        String sql = "delete from "+table.getName()+" where "+table.getPrimaryKey()[0]+"=?";
+        Object paras[][] = new Object[ids.length][1];
+        for (int i = 0; i < ids.length; i++) {
+            paras[i][0] = ids[i];
+        }
+        return Db.batch(sql, paras, ids.length).length;
+    }
+
+
 
 }
